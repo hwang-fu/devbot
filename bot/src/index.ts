390b16ff75ea07257a1d937de4de48d646540ac8
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-25 01:12:34 +0100

    feat: connect bot to Discord

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Bot starting...")
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+} from "discord.js";
+import "dotenv/config";
+
+const token = process.env.DISCORD_TOKEN;
+
+if (!token) {
+  console.error("DISCORD_TOKEN is not set!");
+  process.exit(1);
+}
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Bot is online as ${readyClient.user.tag}`);
+});
+
+client.login(token);
